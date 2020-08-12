9944171dfd1ff0742a4635f4622bceb2339da146
mgbatista <maiara.27bueno@gmail.com>
2020-08-11 22:11:38 -0700

    Quinto dia / Criando banco de dados/tabelas

---
+++ src/database/test.js
@@ -0,0 +1,17 @@
+const Database = require('sqlite-async')
+
+Database.then((db) => {
+    // inserir dados
+
+    proffy = {
+        name: 'Mayk Brito',
+        avatar: 'https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4',
+        whatsapp: '8998877665',
+        bio: 'Instrutor de Educação Física'
+
+    }
+
+
+    // consultar os dados inseridos
+
+})
