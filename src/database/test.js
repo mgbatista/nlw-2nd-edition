19e03cb939161a8c854aafa05cae68f0ee9f50e6
mgbatista <maiara.27bueno@gmail.com>
2020-08-12 21:09:17 -0700

    Quinto dia/criando tabelas

--- src/database/test.js
+++ src/database/test.js
@@ -1,16 +1,37 @@
-const Database = require('sqlite-async')
+const Database = require('./db')
+const createProffy = require('./createProffy')
 
-Database.then((db) => {
+Database.then(async (db) => {
     // inserir dados
 
-    proffy = {
-        name: 'Mayk Brito',
-        avatar: 'https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4',
-        whatsapp: '8998877665',
-        bio: 'Instrutor de Educação Física'
-
+    proffyValue = {
+        name: "Diego Fernandes",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "89995623322",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
     }
 
+    classValue = {
+        subject: "Química",
+        cost: "20",
+        // o proffy id virá pelo banco de dados
+    }
+
+    classScheduleValues = [
+        // class_id virá pelo banco de dados, após cadastrarmos a class
+        {
+            weekday: 1,
+            time_from: 720,
+            time_to: 1220
+        },
+        {
+            weekday: 0,
+            time_from: 520,
+            time_to: 1220  
+        }
+    ]
+
+    // await createProffy (db, {proffyValue, classValue, classScheduleValues})
 
     // consultar os dados inseridos
 
