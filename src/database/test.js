603ba00cc9f2f85290807d08c886d36d5ed386d8
mgbatista <maiara.27bueno@gmail.com>
2020-08-12 21:36:53 -0700

    Quinto dia/Criando tabelas e consultando dados do banco

--- src/database/test.js
+++ src/database/test.js
@@ -35,4 +35,29 @@ Database.then(async (db) => {
 
     // consultar os dados inseridos
 
+    // todos os proffys
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+    //console.log(selectedProffys)
+
+    // consultar as classes de um determinado professor
+    // e trazer junto os dados do professor
+    const selectClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 1;
+    `)
+    //console.log(selectClassesAndProffys)
+
+    // o horário que a pessoa trabalha, por exemplo, é das 8h às 18h
+    // o horário do time_from  (8h) precisa ser menor ou igual ao horário solicitado
+    // o time_to precisa ser acima
+    const selectClassesSchedules = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = 1
+    `)
+
+    console.log(selectClassesSchedules)
+
 })
